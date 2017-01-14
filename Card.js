4a2aded49bbf3181fe9132007db0ccc9a95d5f1f
Pawel Glowacki <eros.z@poczta.fm>
2017-01-14 22:16:15 +0100

    kanban+api

---
+++ Card.js
@@ -0,0 +1,36 @@
+// KLASA KANBAN CARD
+function Card(id, name, level) {
+	var self = this;
+	this.level = level;
+	this.id = id;
+	this.name = name || 'Nie podano nazwy';
+	this.element = createCard();
+
+	function createCard() {
+		var card = $('<li class="card"></li>').addClass('priority' + level);
+		var cardDeleteBtn = $('<button class="btn-delete">x</button>');
+		var cardDescription = $('<p class="card-description"></p>');
+
+		cardDeleteBtn.click(function(){
+			self.removeCard();
+		});		
+
+		card.append(cardDeleteBtn);
+		cardDescription.text(self.name);
+		card.append(cardDescription);
+		return card;
+	}
+}
+
+Card.prototype = {
+	removeCard: function() {
+    	var self = this;
+    	$.ajax({
+      		url: baseUrl + '/card/' + self.id,
+      		method: 'DELETE',
+      		success: function(){
+        		self.element.remove();
+      		}
+    	});
+	}
+}
