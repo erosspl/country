27e8d35d792e21d76d7a1e554308b4d588dbda0a
Pawel Glowacki <eros.z@poczta.fm>
2017-01-18 15:32:07 +0100

    few upgrades

--- scripts.js
+++ scripts.js
@@ -3,28 +3,46 @@ var countriesList = $('#countries');
 
 function searchCountries() {
 	var countryName = $('#country-name').val();
-	if(!countryName) countryName = 'Poland';
+	if(!countryName) {
+    countryName = 'Poland'
+  };
 	$.ajax({
   		url: url + countryName,
   		method: 'GET',
   		success: showCountriesList
-  	});
+  });
 };
 
 function showCountriesList(resp) {
+  $('#body img:last-child').remove();
   countriesList.empty();
-	resp.forEach(function(item){
-		var info = [item.name + ', ' + item.capital];
-   		$('<li>').text('Country and capital city: ' + info).appendTo(countriesList);
-   		$('<li>').text('\n Common languages used: ' + item.languages).appendTo(countriesList);
-   		$('<li>').text('\n Population: ' + item.population).appendTo(countriesList);
-
-	}); 
+  resp.forEach(function(item){
+    if (resp.length !== 1) {
+      var myLi = document.createElement('li');
+      myLi.innerHTML = 'Country:' + ' ' + item.name;
+      countriesList.append(myLi);
+      myLi.addEventListener('click', function(event) {
+        countriesList.empty();
+        $('<li>').text('Country and capital city: ' + item.name + ', ' + item.capital).appendTo(countriesList).append($('<i class="fa fa-building" aria-hidden="true"></i>'));
+        $('<li>').text('Common languages used: ' + item.languages).appendTo(countriesList).append($('<i class="fa fa-commenting-o" aria-hidden="true"></i>'));
+        $('<li>').text('Population: ' + item.population).appendTo(countriesList).append($('<i class="fa fa-users" aria-hidden="true"></i>'));
+        $('#body').append('<img id="flag" src="http://flags.fmcdn.net/data/flags/normal/' + item.alpha2Code.toLowerCase() + '.png" alt="no flag available">');
+      })
+    }
+    else {
+        countriesList.empty();
+        $('<li>').text('Country and capital city: ' + item.name + ', ' + item.capital).appendTo(countriesList).append($('<i class="fa fa-building" aria-hidden="true"></i>'));
+        $('<li>').text('Common languages used: ' + item.languages).appendTo(countriesList).append($('<i class="fa fa-commenting-o" aria-hidden="true"></i>'));
+        $('<li>').text('Population: ' + item.population).appendTo(countriesList).append($('<i class="fa fa-users" aria-hidden="true"></i>'));
+        $('#body').append('<img id="flag" src="http://flags.fmcdn.net/data/flags/normal/' + item.alpha2Code.toLowerCase() + '.png" alt="no flag available">');
+    }
+  });
 };
 
 $('#search').click(searchCountries);
-$( "#country-name" ).keypress(function( event ) {
-  if ( event.which == 13 ) {
+$("#country-name").keypress(function(event) {
+  if (event.which === 13) {
     searchCountries();
   };
 });
+
